d4ed764cdd5aaa67979471fb429332838c0f7754
Mathias/Kei <sui_generis47@protonmail.com>
2020-11-19 15:34:42 +0500

    updates 2

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -1,15 +1,15 @@
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git /usr/share/zsh/plugins/zsh-autosuggestions
 sudo git clone https://github.com/zsh-users/zsh-history-substring-search.git  /usr/share/zsh/plugins/zsh-history-substring-search
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git  /usr/share/zsh/plugins/zsh-syntax-highlighting
-sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git  /usr/share/zsh/plugins/zsh-syntax-highlighting
 sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /usr/share/zsh-theme-powerlevel10k
 
-cp ../config/nvim/* ~/.config/nvim
-cp ../config/sway ~/.config/sway
-cp ../config/ranger ~/.config/ranger
-cp ../config/waybar ~/.config/waybar
-cp ../config/cmus ~/.config/cmus
-cp .zshrc ~/.zshrc
-cp .p10k.zsh ~/.p10k.zsh
+cp -r config/nvim ~/.config/nvim
+cp -r config/sway ~/.config/sway
+cp -r config/ranger ~/.config/ranger
+cp -r config/waybar ~/.config/waybar
+cp -r config/cmus ~/.config/cmus
+cp -r scripts/.zshrc ~/.zshrc
+cp -r scripts/.p10k.zsh ~/.p10k.zsh
 
-curl -fLo ~/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
