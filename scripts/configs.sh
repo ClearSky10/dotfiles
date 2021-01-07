01b3b6b7f4e9ea77b7561430a375153b3049ab69
Mathias/Kei <stofthivi@protonmail.com>
2021-01-07 12:54:15 +0500

    change package installation format

--- scripts/configs.sh
+++ scripts/configs.sh
@@ -1,5 +1,3 @@
-sudo pacman -S --noconfirm --needed git ranger zsh zsh-theme-powerlevel10k zsh-syntax-highlighting zsh-history-substring-search zsh-autosuggestions neovim python-pynvim yarn base-devel iwd
-
 cp -r config/nvim ~/.config/nvim
 cp -r config/sway ~/.config/sway
 cp -r config/ranger ~/.config/ranger
@@ -10,9 +8,3 @@ cp -r scripts/.p10k.zsh ~/.p10k.zsh
 
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -sic
-cd ..
-rm -rf yay
--- scripts/desktop.sh
+++
@@ -1,1 +0,0 @@
-sudo pacman -S sway waybar wl-clipboard jq cmus firefox telegram-desktop foot neomutt
